5701d24d3920582994dc4efc5f355f2fcbe7e50a
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-09-08 10:40:45 -0500

    renamed index.cshtml so its harder to get to

--- server/Program.cs
+++ server/Program.cs
@@ -1,9 +1,68 @@
+using System.Diagnostics;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+bool IsNotOpen(WebSocket ws){
+    return (ws.State != WebSocketState.Open);
+}
+async Task<bool> SendMessage(WebSocket ws, string message){
+    byte[] bytes = Encoding.UTF8.GetBytes(message);
+    ArraySegment<byte> arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+    if(IsNotOpen(ws)) return false;
+    await ws.SendAsync(arraySegment,
+                    WebSocketMessageType.Text,
+                    true,
+                    CancellationToken.None);
+    
+    return true;
+}
+
+async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage){
+    byte[] buffer = new byte[1024 * 4];
+    
+    while(IsNotOpen(socket)){
+        var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        handleMessage(result, buffer); 
+    }
+}
+
+app.UseWebSockets();
+app.Map("/ws", async context => {
+    if (!context.WebSockets.IsWebSocketRequest){
+        context.Response.StatusCode = (int)HttpStatusCode.UpgradeRequired;
+        return;
+    }
+
+    using var ws = await context.WebSockets.AcceptWebSocketAsync();
+    await ReceiveMessage(ws, async (result, buffer)=>{
+        Debug.WriteLine("Message received");
+        if(result.MessageType == WebSocketMessageType.Text){
+                    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    await SendMessage(ws, message);
+        }
+
+
+    });
+    while(true){
+        
+        var message = DateTime.Now.ToString("HH:mm:ss");
+        var sent = await SendMessage(ws, message);
+        if(!sent)break;
+        Debug.WriteLine(message);
+        
+        Thread.Sleep(1000);
+    }
+
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
