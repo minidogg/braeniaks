4154189adef3ab97d2ba55a77d83f36306bba177
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-09-22 16:48:25 -0500

    Add thing for setting the server url.

--- web/js-src/entry.js
+++ web/js-src/entry.js
@@ -7,6 +7,8 @@ const pages = {
     "/quiz.html": Quiz.Page
 }
 
+if(localStorage.getItem("serverURL")==null)localStorage.setItem("serverURL", "https://localhost:7026/")
+
 if(pages[window.location.pathname]){
     let pageHTML = pages[window.location.pathname]();
 
